0b55120cd7f3ba333806ee812c85fab904d7897b
Adeeb Ahmed Khan <adeebahmed_khan@epam.com>
2025-06-20 09:02:54 +0000

    Updated outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,8 +1,3 @@
-# output "acr_login_url" {
-#   description = "The login URL of the Azure Container Registry."
-#   value       = module.acr.login_url
-
-# }
 output "sql_server_fqdn" {
   description = "The fully qualified domain name of the SQL server."
   value       = module.sql.sql_server_fqdn
@@ -13,9 +8,5 @@ output "web_app_default_hostname" {
   value       = module.web_app.web_app_default_hostname
 
 }
-# output "aks_lb_ip" {
-#   description = "The IP address of the AKS load balancer."
-#   value       = module.aks_lb.ip_address
 
-# }
 
